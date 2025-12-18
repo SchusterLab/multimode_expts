d8330ef3cf46285a47d48df36356d928914c4fc4
guan <gzhwang@stanford.edu>
2025-12-17 23:16:12 -0800

    experiment classes now ignore top level modules!

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,40 +1,51 @@
-import importlib, inspect
-import os,os.path
+import importlib
+import inspect
+import os
 import sys
 
 """
 module_path: parent directory path with . as subfile dividers
 f: filename to import modules from
 """
+
+
 def import_modules_from_files(module_path, f):
-    if f[0]!="_" and f[0]!=".":
+    if f[0] != "_" and f[0] != ".":
         module_name = module_path + "." + f.split(".")[0]
         m = importlib.import_module(module_name)
-        print('imported', module_name)
+        # print("imported", module_name)
         for name, obj in inspect.getmembers(m):
             if inspect.isclass(obj):
-                setattr(thismodule,name,getattr(m, name))
+                setattr(thismodule, name, getattr(m, name))
         del m
         del name
         del obj
 
+
 path = __path__[0]
 thismodule = sys.modules[__name__]
-files=os.listdir(path)
+files = os.listdir(path)
 module_path = os.path.split(path)[-1]
+skipped_files = []
 for f in files:
     fpath = os.path.join(path, f)
-    if f[0]=="_" or f[0]==".": continue
+    if f[0] == "_" or f[0] == ".":
+        continue
 
-    print('importing from path ', fpath)
     if os.path.isdir(fpath):
+        print("Importing all classes from ", fpath)
         subfiles = os.listdir(fpath)
         submodule_path = module_path + "." + os.path.split(fpath)[-1]
         for subf in subfiles:
             import_modules_from_files(submodule_path, subf)
     else:
-        import_modules_from_files(module_path, f)
+        # stick to the convention that only sub modules contain Program and Experiment classes
+        # top level modules are for generic, shared utilities
+        skipped_files.append(f)
+
+print(f'Skipped top-level files {skipped_files}')
 
 del f
 del thismodule
 del files
+
