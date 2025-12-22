88c3e05ad8acfd4b37fd46f7c85989a0782b64fc
Gaberber <cnhajzwgz@gmail.com>
2025-12-22 15:44:02 -0800

    also add ns_port to slab/im to maintain compatibility

--- slab/instruments/instrumentmanager.py
+++ slab/instruments/instrumentmanager.py
@@ -30,12 +30,13 @@ class InstrumentManager(dict):
     keeps track of listed instruments and their settings
     :param config_path: Path to configuration file
     """
-    def __init__(self, config_path=None, server=False, ns_address=None):
+    def __init__(self, config_path=None, server=False, ns_address=None, ns_port=None):
         """Initializes InstrumentManager using config_path if available"""
         dict.__init__(self)
         self.config_path = config_path
         self.config = None
         self.ns_address = ns_address
+        self.ns_port = ns_port
         #self.instruments={}
         if not server and Pyro4Loaded:
                 try:
@@ -108,8 +109,8 @@ class InstrumentManager(dict):
         host=s.getsockname()[0]
         #host=socket.gethostbyname(socket.gethostname())
         daemon = Pyro4.Daemon(host=host)
-        ns = Pyro4.locateNS(self.ns_address)
-     
+        ns = Pyro4.locateNS(self.ns_address, self.ns_port)
+
         for instrument in instruments:
             uri = daemon.register(instrument)
             ns.register(instrument.name, uri)
@@ -125,7 +126,7 @@ class InstrumentManager(dict):
         daemon.requestLoop()
 
     def connect_proxies(self):
-        ns = Pyro4.locateNS(self.ns_address)
+        ns = Pyro4.locateNS(self.ns_address, self.ns_port)
         for name, uri in list(ns.list().items()):
             self[name] = Pyro4.Proxy(uri)
 
@@ -159,7 +160,7 @@ class InstrumentManager(dict):
     def clean_nameserver(self):
         """Checks to make sure all of the names listed
         in server are really there"""
-        ns = Pyro4.locateNS(self.ns_address)
+        ns = Pyro4.locateNS(self.ns_address, self.ns_port)
         for name, uri in list(ns.list().items()):
             try:
                 proxy=Pyro4.Proxy(uri)
