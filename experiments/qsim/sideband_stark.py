a6262778cfb29412619fd321aa86c66d1bd348b1
jonginnyun <alyuuv@snu.ac.kr>
2026-09-09 10:35:36 -0700

    qsim: Migrated ``SidebandStarkAmplificationModifiedProgram_old`` to ``SidebandStarkAmplificationProgram``.

--- experiments/qsim/sideband_stark.py
+++ experiments/qsim/sideband_stark.py
@@ -63,9 +63,13 @@ class SidebandStarkAmplificationProgram(QsimBaseProgram):
     stor_B
     n_pulse: Nx pulses on stor B 
     advance_phase: phase of the last pulse on stor_A
+    scramble_sync_cycles: The cycle between floquet pulses.
+    include_10cycles_buffer: Whether to include ``scramble_sync_cycles`` for stor_B
+    include_10cycles_buffer_in_pi_half: Whether to include ``scramble_sync_cycles`` for stor_A
     """
 
     def core_pulses(self):
+        _scramble_sync_cycles = self.cfg.expt.get("scramble_sync_cycles", 10)
         i_storA = self.cfg.expt.stor_A - 1
         i_storB = self.cfg.expt.stor_B - 1
         m1s_kwarg_A = self.m1s_kwargs[i_storA]
@@ -84,14 +88,10 @@ class SidebandStarkAmplificationProgram(QsimBaseProgram):
         self.set_pulse_registers(**m1s_kwarg_A)
         for i in range(pi_frac_A // 2):
             self.pulse(ch_A)
+            if self.cfg.expt.get("include_10cycles_buffer", False) and self.cfg.expt.get("include_10cycles_buffer_in_pi_half", False):
+                self.sync_all(_scramble_sync_cycles)
         self.sync_all()
         
-        # # Apply a 2pi * n_pulse gate on stor_B
-        # self.set_pulse_registers(**m1s_kwarg_B)
-        # for i in range(n_pulse_B * 2 * pi_frac_B):
-        #     self.pulse(ch_B)
-        # advance_phase_A = self.deg2reg(n_pulse_B * pi_frac * self.cfg.expt.advance_phase)
-        # self.sync_all()
 
         # Apply a (pi/12, -pi/12) * n_pulse gate on stor_B
         phase = 0
@@ -104,6 +104,8 @@ class SidebandStarkAmplificationProgram(QsimBaseProgram):
                 phase = phase % 360
                 _phase_reg = self.deg2reg(phase, gen_ch=ch_B)
                 self.safe_regwi(channel_page_B, r_phase_B, _phase_reg)
+                if self.cfg.expt.get("include_10cycles_buffer", False):
+                    self.sync_all(_scramble_sync_cycles)
         advance_phase_A = self.deg2reg(2 * n_pulse_B * self.cfg.expt.advance_phase)
         self.sync_all()
         
@@ -113,6 +115,8 @@ class SidebandStarkAmplificationProgram(QsimBaseProgram):
         self.set_pulse_registers(**m1s_kwarg_A_advanced)
         for i in range(pi_frac_A // 2):
             self.pulse(m1s_kwarg_A_advanced['ch'])
+            if self.cfg.expt.get("include_10cycles_buffer", False) and self.cfg.expt.get("include_10cycles_buffer_in_pi_half", False):
+                self.sync_all(_scramble_sync_cycles)
         self.sync_all()
 
 class SidebandStarkAmplificationExperiment(QsimBaseExperiment):
